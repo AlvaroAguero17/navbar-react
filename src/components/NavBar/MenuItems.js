bab75e2e35fc25bfb6c1c9a3bc9cba25291ea579
Alvaro <alvaroawero10@gmail.com>
2020-10-22 10:52:00 -0300

    navbar with reactjs

---
+++ src/components/NavBar/MenuItems.js
@@ -0,0 +1,27 @@
+export const MenuItems = [
+    {
+        title: 'Inicio',
+        url: '#',
+        cName: 'nav-links'
+    },
+    {
+        title: 'Servicios',
+        url: '#',
+        cName: 'nav-links'
+    },
+    {
+        title: 'Productos',
+        url: '#',
+        cName: 'nav-links'
+    },
+    {
+        title: 'Contacto',
+        url: '#',
+        cName: 'nav-links'
+    },
+    {
+        title: 'Loguearse',
+        url: '#',
+        cName: 'nav-links-mobile'
+    }
+]
